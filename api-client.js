09abc4d8276dfb7f27d9b6de998b262a77425113
Bart Kuipers <bartkuipers88@gmail.com>
2020-06-27 12:12:46 +0200

    added comments, added responsiveness, change date format to european, cleaned up code

--- api-client.js
+++ api-client.js
@@ -1,8 +1,7 @@
+//GET data from database. Added due_date as object key
 const getData = async () => {
     const response = await fetch('https://to-dolist-e1881.firebaseio.com/Apps/Tasklist.json');
-
     const data = await response.json();
-
     let tasks = Object.keys(data).map(key => ({
         id: key,
         description: data[key].description,
@@ -10,17 +9,11 @@ const getData = async () => {
         due_date: data[key].due_date
     }));
     tasks.sort((a, b) => (a.due_date > b.due_date) ? 1 : -1)
-
-
+    //Calls buildList function with tasks array as argument.
     buildList(tasks);
-
-    // just a test
-    console.log(new Date(`${(tasks[0].due_date)}`).getTime() / 1000)
-    console.log(today.getTime() / 1000)
-
-
 }
 
+//Function to update task name via prompt, and pushes to database with PUT request
 const updateTask = async (id, taskDescription, duedate) => {
     const taskPrompt = prompt("Please change your task description", taskDescription);
     const newTaskObject = `{ "description": "${taskPrompt}", "done": false, "due_date": "${duedate}" }`
@@ -28,6 +21,7 @@ const updateTask = async (id, taskDescription, duedate) => {
     getData();
 }
 
+//Function to update task status via PUT request based on user clicking checkbox
 const updateTaskStatus = async (id, taskStatus, taskDescription, duedate) => {
     if (taskStatus === true) {
         const newStatusObject = `{ "description": "${taskDescription}", "done": false, "due_date":"${duedate}" }`
@@ -41,9 +35,7 @@ const updateTaskStatus = async (id, taskStatus, taskDescription, duedate) => {
     }
 };
 
-
-
-
+//Function to delete task with DELETE request
 const deleteTask = async (id) => {
     await fetch(`https://to-dolist-e1881.firebaseio.com/Apps/Tasklist/${id}.json`, { method: 'DELETE' })
     getData();
--- app.js
+++ app.js
@@ -8,10 +8,7 @@ const nextTaskDate = document.querySelector(".nexttaskdate");
 const body = document.querySelector("body");
 const deleteCompleted = document.querySelector("#removecompletedbutton");
 
-const showDate = () => {
-    return dateInput.value;
-}
-
+//Checks for input in text and date fields. Sends post request with user input. Starts getData function to retrieve updated data.
 document.querySelector("#addTask").addEventListener("click", async () => {
     if (dateInput.value == "" || taskInput.value == "") {
         alert("Please enter a task and a due date")
@@ -25,32 +22,38 @@ document.querySelector("#addTask").addEventListener("click", async () => {
     getData();
 })
 
-getData();
+//Function to rewrite standard date format YYYY-MM-DD to DD/MM/YYYY
+const reverseDate = (inputString) => {
+    const oldDate = Array.from(inputString);
+    const newDateNotation = oldDate[8] + oldDate[9] + "/" + oldDate[5] + oldDate[6] + "/" + oldDate[0] + oldDate[1] + oldDate[2] + oldDate[3];
+    return newDateNotation;
+}
 
+//Function to display to-do list items based on data from getData function (in api-client.js)
 const buildList = (tasks) => {
     openTasksContainer.innerHTML = "";
     completedTasksContainer.innerHTML = "";
     tasks.forEach(task => {
+
+        const taskItemContainer = document.createElement('div');
         const taskItem = document.createElement('input');
         const taskName = document.createElement('label');
-        const deleteButton = document.createElement('img');
-        const editButton = document.createElement('img');
-        const taskItemContainer = document.createElement('div');
         const dueDate = document.createElement('p');
-        dueDate.className = "duedate";
+        const editButton = document.createElement('img');
+        const deleteButton = document.createElement('img');
+
 
         taskItemContainer.className = "taskitemcontainer";
 
         taskItem.type = "checkbox";
         taskItem.name = task.id;
         taskItem.className = "checkbox"
+
         taskName.for = task.id;
         taskName.innerHTML = task.description;
 
-        dueDate.innerHTML = task.due_date;
-
-
-
+        dueDate.className = "duedate";
+        dueDate.innerHTML = reverseDate(task.due_date);
 
         deleteButton.src = 'delete.svg';
         deleteButton.className = "deletebutton";
@@ -60,18 +63,14 @@ const buildList = (tasks) => {
         taskItemContainer.appendChild(taskItem);
         taskItemContainer.appendChild(taskName);
         taskItemContainer.appendChild(dueDate);
-
-
-
-
-
+        //If item is done, append to completedTasksContainer and set class to 'completedtask' for styling
         if (task.done === true) {
-
             taskItemContainer.appendChild(deleteButton);
             taskItem.checked = true;
             taskName.className = "completedtask";
             completedTasksContainer.appendChild(taskItemContainer);
         } else {
+            //if task status === not done, if statement checks how far in the future a task deadline is. < 2 days = urgent(purple), < 1 week = moderate(blue), > 1 week = noturgent(green)
             if (((new Date(`${task.due_date}`).getTime() / 1000) - (today.getTime() / 1000)) < 172800) {
                 taskItemContainer.classList += " urgent"
             } else if (((new Date(`${task.due_date}`).getTime() / 1000) - (today.getTime() / 1000)) < 604800) {
@@ -80,13 +79,12 @@ const buildList = (tasks) => {
                 taskItemContainer.classList += " noturgent"
             }
 
-
             taskItemContainer.appendChild(editButton);
             taskItemContainer.appendChild(deleteButton);
             taskName.className = "opentask";
             openTasksContainer.appendChild(taskItemContainer);
-
         }
+        //add event listeners for delete and edit buttons, and checkbox
         deleteButton.addEventListener("click", () => {
             deleteTask(task.id);
         })
@@ -99,17 +97,21 @@ const buildList = (tasks) => {
 
     });
 
+    //creates array of only open tasks
     const nextToDo = tasks.filter(task => {
         return task.done == false;
     })
-    console.log(nextToDo)
+
+    //Displays current most urgent task in right side of page
     nextTask.innerHTML = nextToDo[0].description;
-    nextTaskDate.innerHTML = nextToDo[0].due_date;
+    nextTaskDate.innerHTML = reverseDate(nextToDo[0].due_date);
+
+    //Creates array of completed tasks
     const tasksToRemove = tasks.filter(task => {
         return task.done == true;
     })
-    console.log(tasksToRemove);
 
+    //Calls the delete task function for each item in the array of completed items. 
     deleteCompleted.addEventListener("click", () => {
         tasksToRemove.forEach(task => {
             deleteTask(task.id);
@@ -117,11 +119,11 @@ const buildList = (tasks) => {
     })
 }
 
+//Briefly turns the screen green to give feedback to user when completing a task.
 const reward = () => {
-    console.log("compelted something")
-    body.className = "completedtaskbody";
-    setInterval(() => { body.className = "" }, 2000);
+    body.classList += " completedtaskbody";
+    setInterval(() => { body.classList.remove("completedtaskbody") }, 2000);
 }
 
-
-//To chamge existing item: Sent put request https://to-dolist-e1881.firebaseio.com/Apps/Tasklist/-MAgEjRYnc6gWClLjCmN.json
+//Calls getData function to populate the list when the page loads
+getData();
